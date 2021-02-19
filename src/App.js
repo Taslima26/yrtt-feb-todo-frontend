186d2031e08e6e18bb022056dfee30d030c25c5f
taslima26 <banglawalas@hotmail.co.uk>
2021-02-19 16:43:43 +0000

    managed to dispaly tasks using map

--- src/App.js
+++ src/App.js
@@ -4,8 +4,42 @@ import AddTaskItem from "./components/AddTaskItem/AddTaskItem";
 import TaskItem from "./components/TaskItem/TaskItem";
 
 function App() {
-  const [tasks, setTasks] = useState();
-
+  const [tasks, setTasks] = useState([
+    {
+      text: "Do Sainsbury shop",
+      completed: false,
+      date: "2021-02-22",
+      id: "001",
+    },
+    {
+      text: "Make something nice to eat",
+      completed: false,
+      date: "2021-02-22",
+      id: "002",
+    },
+    {
+      text: "Dry the washing",
+      completed: false,
+      date: "2021-02-19",
+      id: "003",
+    },
+    {
+      text: "work with Khadijah",
+      completed: true,
+      date: "2021-02-19",
+      id: "003",
+    },
+    {
+      text: "Buy some cake",
+      completed: true,
+      date: "2021-02-19",
+      id: "004",
+    },
+  ]);
+  const incompleteTask = tasks.filter((task) => !task.completed);
+  const completeTask = tasks.filter((task) => task.completed);
+  console.log(completeTask);
+  console.log(incompleteTask);
   /*[
     {text:"Do Sainsbury shop",completed:false,date:"2021-02-22"},
     {text:"Make something nice to eat,campleted:false,date:"2021-02-22"},
@@ -21,9 +55,7 @@ function App() {
           <AddTaskItem />
           <p className="outstanding-tasks">You have 3 tasks to complete</p>
         </header>
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
+        <TaskItem completeTask={completeTask} incompleteTask={incompleteTask} />
       </div>
     </div>
   );
--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./TaskItem.css";
 
-const TaskItem = () => {
+const TaskItem = ({ completeTask, incompleteTask }) => {
+  console.log(completeTask);
   return (
     <div>
       <main className="all-tasks">
         <section className="incomplete-tasks">
           <h2 className="heading">Tasks to do:</h2>
           <ul className="task-list">
-            <li className="task">
-              <p className="task__text">This is a task that needs to be done</p>
-              <button className="button">Complete</button>
-              <button className="button">Delete</button>
-            </li>
+            {completeTask.map((task) => (
+              <li className="task">
+                <p className="task__text">{task.text}</p>
+                <button className="button">Complete</button>
+                <button className="button">Delete</button>
+              </li>
+            ))}
           </ul>
         </section>
         <section className="complete-tasks"></section>
