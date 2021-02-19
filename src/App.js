ec172b0be3ee888057e543418b80a70259bb10af
taslima26 <banglawalas@hotmail.co.uk>
2021-02-19 16:07:12 +0000

    seprated out componet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 import AddTaskItem from "./components/AddTaskItem/AddTaskItem";
 import TaskItem from "./components/TaskItem/TaskItem";
 
 function App() {
+  const [tasks, setTasks] = useState();
+
+  /*[
+    {text:"Do Sainsbury shop",completed:false,date:"2021-02-22"},
+    {text:"Make something nice to eat,campleted:false,date:"2021-02-22"},
+    {text:"Dry the washing",completed:false,date:"2021-02-19"}
+  ]
+
+  */
   return (
     <div className="App">
       <div className="body">
